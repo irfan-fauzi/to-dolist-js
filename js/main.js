2b845e13e17d0723368e12f5f509211941942fda
irfan-fauzi <irfanwintech@yahoo.com>
2020-03-25 01:06:45 +0700

    js time use class

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const addButton = document.querySelector('.addButton');
+let inputValue = document.querySelector('.input');
+const container = document.querySelector('.container');
+
+class item {
+    constructor(itemName) {
+        // create the item div
+
+    }
+}
